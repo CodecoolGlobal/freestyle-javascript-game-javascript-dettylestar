96eb01c24a2d44cdb0d699294c62dd0b242151e0
Makó Dávid <david@Mako-MacBook-Air.local>
2022-09-06 12:41:25 +0200

    add the half of the collision detection

--- game.js
+++ game.js
@@ -5,3 +5,19 @@ function initGame() {
     // Your game can start here, but define separate functions, don't write everything in here :)
 
 }
+function collisionDetection() {
+    let ball = document.querySelector('#ball').getBoundingClientRect();
+    let wall = document.querySelector('.table').getBoundingClientRect();
+    let leftPlayer = document.querySelector('#left-player').getBoundingClientRect();
+    let rightPlayer = document.querySelector('#right-player').getBoundingClientRect();
+    if (ball.top >= wall.top) {
+        return 'y'
+    } else if (ball.bottom >= wall.bottom) {
+        return 'x'
+    } else if ((leftPlayer.right >= ball.left && ball.top <= leftPlayer.bottom && ball.bottom >= leftPlayer.top)()) {
+
+
+    }
+}
+
+collisionDetection();
